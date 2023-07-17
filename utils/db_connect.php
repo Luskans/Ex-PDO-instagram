dc38584397244e8dc347f2026cc210298fd9cbf9
HHOZKAN <ozkn.haci@gmail.com>
2023-07-17 16:15:06 +0200

    pb de co

--- utils/db_connect.php
+++ utils/db_connect.php
@@ -1,2 +1,16 @@
+<?php
+
+
+$dns = 'mysql:hpst=localhost;dbname=instakilo';
+$user ='root';
+$password = '';
+
+try{
+    $db = new PDO($dns, $user, $password);
+}
+catch (Exception $message){
+    echo "Error:"  . "<pre> $message </pre>";
+}
+
 
 ?>
