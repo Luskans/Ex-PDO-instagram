17ebf65aedc0479e47a2c96d0676b0135ec755a3
HHOZKAN <ozkn.haci@gmail.com>
2023-07-18 10:58:44 +0200

    add

--- login.php
+++ login.php
@@ -38,5 +38,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <form class="" action="login.php" method="POST">
     <input type="text" name="email" placeholder="E-mail" required><br>
     <input type="password" name="password" placeholder="Password" required><br><br>
-    <button type="submit" name="submit">Login</button>
+    <button type="submit" name="submit">Login</button> <br> <br>
+    <button onclick="window.location.href = './signup.php';"> Sign up </button>
 </form>
