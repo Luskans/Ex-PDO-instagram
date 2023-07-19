61c1addfb8c8ed74522363733095c8170b297bee
Luskans <sylvain.migne@gmail.com>
2023-07-19 11:42:52 +0200

    change of header, and css for image display

--- image.php
+++ image.php
@@ -3,67 +3,70 @@ include_once('./partials/header.php');
 require('./process/get_image.php');
 ?>
 
-<main>
-    <div class="container">
-        <section>
-            <article>
-                <img src="<?=$image['link']?>">
-            </article>
-            <aside>
-                <p> <?=$image['date']?> </p>
+<main class="img_display d-flex">
+    <section>
+        <article>
+            <img src="<?=$image['link']?>">
+        </article>
+        <aside class="d-flex justify-content-around align-items-center">
+            <p> <?=$image['date']?> </p>
+            <div class="d-flex flex-column align-items-center">
                 <p> <?=$image['nb_views']?> </p>
+                <p> Vues </p>
+            </div>
+            <div class="d-flex flex-column align-items-center">
                 <p> <?=$image['nb_likes']?> </p>
-            </aside>
-        </section>
-        <section>
-            <article>
+                <p> Likes </p>
+            </div>
+        </aside>
+    </section>
+    <section>
+        <article>
+            <div>
                 <div>
-                    <div>
-                        <a href="./profil.php?id=<?=$profil['id_user']?>"> <img src="<?=$profil['avatar_link']?>"> </a>
-                    </div>
-                    <p> <?= $profil['name'] ?> </p>
+                    <a href="./profil.php?id=<?=$profil['id_user']?>"> <img src="<?=$profil['avatar_link']?>"> </a>
                 </div>
-                <div>
-                    <p> <?= $image['description'] ?> </p>
-                </div>
-            </article>
-            <article>
-                <?php
-                    foreach($comments as $comment) {
-                        
-                        // On récupère tous du profil selon l'id_user du commentaire
-                        $request = $db->prepare('SELECT * FROM profils WHERE id_user = :id_user');
-                        $request->execute([
-                            'id_user' => $comment['id_user']
-                        ]);
-                        $autor_comment = $request->fetch();
+                <p> <?= $profil['name'] ?> </p>
+            </div>
+            <div class="mb-5 border-bottom">
+                <p> <?= $image['description'] ?> </p>
+            </div>
+        </article>
+        <article>
+            <?php
+                foreach($comments as $comment) {
+                    
+                    // On récupère tous du profil selon l'id_user du commentaire
+                    $request = $db->prepare('SELECT * FROM profils WHERE id_user = :id_user');
+                    $request->execute([
+                        'id_user' => $comment['id_user']
+                    ]);
+                    $autor_comment = $request->fetch();
 
-                        // Si le profil n'a pas de photo, on en met une par défaut
-                        $avatar = ($autor_comment['avatar_link'] == NULL) ? "./uploads/avatars/user1.png" : $autor_comment['avatar_link'];
-                ?>
-                        <div>
-                            <div>
-                                <div>
-                                    <a href="./profil.php?id=<?=$autor_comment['id_user']?>"> <img src="<?=$avatar?>"> </a>
-                                </div>
-                                <p> <a href="./profil.php?id=<?=$autor_comment['id_user']?>"> <?=$autor_comment['name']?> </a> </p>
-                                <p> <?=$comment['date']?> </p>
+                    // Si le profil n'a pas de photo, on en met une par défaut
+                    $avatar = ($autor_comment['avatar_link'] == NULL) ? "./uploads/avatars/user1.png" : $autor_comment['avatar_link'];
+            ?>
+                    <div class="d-flex flex-column">
+                        <div class="d-flex gap-3">
+                            <div class="avatar">
+                                <a href="./profil.php?id=<?=$autor_comment['id_user']?>"> <img src="<?=$avatar?>"> </a>
                             </div>
-                            <div>
-                                <p> <?=$comment['comment']?> </p>
-                            </div>
+                            <p> <a href="./profil.php?id=<?=$autor_comment['id_user']?>"> <?=$autor_comment['name']?> </a> </p>
+                            <p> <?=$comment['date']?> </p>
                         </div>
-                    <?php } ?>
-            </article>
+                        <div class="mb-5">
+                            <p> <?=$comment['comment']?> </p>
+                        </div>
+                    </div>
+                <?php } ?>
+        </article>
 
-            <form action="./process/add_comment.php?id=<?=$image['id']?>" method="post">
-                <textarea name="comment" required></textarea>
-                <button type="submit">Envoyer</button>
-            </form>
-        </section>
-    </div>
+        <form action="./process/add_comment.php?id=<?=$image['id']?>" method="post">
+            <textarea name="comment" required></textarea>
+            <button type="submit">Envoyer</button>
+        </form>
+    </section>
 </main>
-
 
 
 
--- partials/header.php
+++ partials/header.php
@@ -4,19 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Instakilo</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="./assets/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
-<body>
+<body class="d-flex">
     <header>
-        <nav class="parent">
-            <div class="div1"> <a href="#" class="logo"> INSTAKILO </a> </div>
-            <div class="div2"> <a href="../index.php"> <img src="../assets/image/accueil.png" alt="Icone Accueil">Accueil</a></div>
-            <div class="div3"> <a href="#"> <img src="../assets/image/2205851-icone-en-forme-de-loupe-vectoriel-removebg-preview.png" alt="rechercher">  Recherche</a></div>
-            <div  class="div4"> <a href="#"><img src="../assets/image/iconemessage.png" alt="">Messages</a></div>
-            <div class="div5"> <a href="#"><img src="../assets/image/notifications.png" alt="">Notifications</a> </div>
-            <div class="div6"> <a href="#"><img src="../assets/image/ajout.png" alt="">Créer/Publier</a> </div>
-            <div class="div7"> <a href="../profil.php"><img src="" alt="">Profil</a></div>
-            <div class="div8"> <a href=""><img src="../assets/image/Hamburger_icon.svg.png" alt="menu hamburger" class="menuhamburger"> Plus </a> </div>
+        <nav class="parent d-flex flex-column gap-3">
+            <div class="div1">
+                <a class="d-flex" href="#" class="logo"> INSTAKILO </a>
+            </div>
+            <div class="div2">
+                <a class="d-flex" href="../index.php">
+                    <img src="../assets/image/accueil.png" alt="Icone Accueil">
+                    <p> Accueil </p>
+                </a>
+            </div>
+            <div class="div3">
+                <a class="d-flex" href="#">
+                    <img src="../assets/image/2205851-icone-en-forme-de-loupe-vectoriel-removebg-preview.png" alt="rechercher">
+                    <p> Recherche </p>
+                </a>
+            </div>
+            <div class="div4">
+                <a class="d-flex" href="#">
+                    <img src="../assets/image/iconemessage.png" alt="">
+                    <p> Messages </p>
+                </a>
+            </div>
+            <div class="div5">
+                <a class="d-flex" href="#">
+                    <img src="../assets/image/notifications.png" alt="">
+                    <p> Notifications </p>
+                </a> 
+            </div>
+            <div class="div6">
+                <a class="d-flex" href="#">
+                    <img src="../assets/image/ajout.png" alt="">
+                    <p> Créer/Publier </p>
+                </a> 
+            </div>
+            <div class="div7">
+                <a class="d-flex" href="../profil.php">
+                    <img src="" alt="">
+                    <p> Profil </p>
+                </a>
+            </div>
+            <div class="div8">
+                <a class="d-flex" href="">
+                    <img src="../assets/image/Hamburger_icon.svg.png" alt="menu hamburger" class="menuhamburger">
+                    <p> Plus </p>
+                </a> 
+            </div>
         </nav>
     </header>
