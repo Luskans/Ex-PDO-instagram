f28043b0922a3c75f1a99a53a87bb54a98444989
HHOZKAN <ozkn.haci@gmail.com>
2023-07-19 11:50:55 +0200

    index style

--- partials/header.php
+++ partials/header.php
@@ -11,14 +11,14 @@
 
 <body>
     <header>
-        <nav class="parent">
-            <div class="div1"> <a href="#" class="logo"> INSTAKILO </a> </div>
-            <div class="div2"> <a href="../index.php"> <img src="../assets/image/accueil.png" alt="Icone Accueil">Accueil</a></div>
-            <div class="div3"> <a href="#"> <img src="../assets/image/2205851-icone-en-forme-de-loupe-vectoriel-removebg-preview.png" alt="rechercher">  Recherche</a></div>
-            <div  class="div4"> <a href="#"><img src="../assets/image/iconemessage.png" alt="">Messages</a></div>
-            <div class="div5"> <a href="#"><img src="../assets/image/notifications.png" alt="">Notifications</a> </div>
-            <div class="div6"> <a href="#"><img src="../assets/image/ajout.png" alt="">Créer/Publier</a> </div>
-            <div class="div7"> <a href="../profil.php"><img src="" alt="">Profil</a></div>
-            <div class="div8"> <a href=""><img src="../assets/image/Hamburger_icon.svg.png" alt="menu hamburger" class="menuhamburger"> Plus </a> </div>
+        <nav class="">
+            <div class="div"> <a href="#" class="logo"> INSTAKILO </a> </div>
+            <div class="div"> <a href="../index.php"> <img src="../assets/image/accueil.png" alt="Icone Accueil">Accueil</a></div>
+            <div class="div"> <a href="#"> <img src="../assets/image/2205851-icone-en-forme-de-loupe-vectoriel-removebg-preview.png" alt="rechercher">  Recherche</a></div>
+            <div  class="div"> <a href="#"><img src="../assets/image/iconemessage.png" alt="">Messages</a></div>
+            <div class="div"> <a href="#"><img src="../assets/image/notifications.png" alt="">Notifications</a> </div>
+            <div class="div"> <a href="#"><img src="../assets/image/ajout.png" alt="">Créer/Publier</a> </div>
+            <div class="div"> <a href="../profil.php"><img src="" alt="">Profil</a></div>
+            <div class="div"> <a href=""><img src="../assets/image/Hamburger_icon.svg.png" alt="menu hamburger" class="menuhamburger"> Plus </a> </div>
         </nav>
     </header>
